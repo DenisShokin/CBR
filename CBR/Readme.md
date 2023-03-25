fb171697421aa2fd8b8c5612f60513591f200b10
U_M1BKT <DShokin@alfabank.ru>
2023-03-25 14:49:37 +0300

    add Balance.xml validate test

---
+++ CBR/Readme.md
@@ -0,0 +1,11 @@
+# Тестовое задание
+
+### Баг-репорты доступны по [ссылке](https://docs.google.com/spreadsheets/d/1zq7UkJwgAoyp8M1UnnpYjtMbFgJwACVzDhPGRilJyTk/edit#gid=0).
+
+### Валидация XML файла (автотесты)
+Реализована возможность валидации xml по xsd схеме.
+XML файл для проверки требуется положить в CBR/src/test/resources/xml, задать наименование Balance.xml
+XSD файл для проверки требуется положить в CBR/src/test/resources/xsd , задать наименование schema.xsd
+
+Выполнить команду gradle ```gradle test```
+Отчет доступен 
---
+++ CBR/src/main/java/utils/validator/XmlValidator.java
@@ -0,0 +1,51 @@
+package utils.validator;
+
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class XmlValidator {
+
+    public static List<SAXParseException> getXmlValidateErrors(String xsdPath, String xmlPath) throws SAXException, IOException {
+        SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema schema = factory.newSchema(new File(xsdPath));
+        Validator validator = schema.newValidator();
+        List<SAXParseException> errors = new ArrayList<>();
+
+        validator.setErrorHandler(new ErrorHandler() {
+            @Override
+            public void warning(SAXParseException e) {
+                errors.add(e);
+            }
+
+            @Override
+            public void error(SAXParseException e) {
+                errors.add(e);
+            }
+
+            @Override
+            public void fatalError(SAXParseException e) {
+                errors.add(e);
+            }
+        });
+
+        validator.validate(new StreamSource(new File(xmlPath)));
+
+        if (errors.size() > 0) {
+            return errors;
+        } else {
+            return null;
+        }
+    }
+
+}
